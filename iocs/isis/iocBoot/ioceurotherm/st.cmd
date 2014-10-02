77f7f61f3aabba960fa4806c88ab570103a09c73
Dominic Oram <dominic.oram@stfc.ac.uk>
2014-10-02 17:25:26 +0000

    Added the ReadASCII driver to eurotherm to ramp SPs and PIDs
    Former-commit-id: 3ce16fcd7bb9d344fc0d5774f2a5ed01f035c3df

--- iocs/isis/iocBoot/ioceurotherm/st.cmd
+++ iocs/isis/iocBoot/ioceurotherm/st.cmd
@@ -7,7 +7,7 @@
 
 epicsEnvSet "IOCNAME" "EUROTHERM"
 epicsEnvSet "STREAM_PROTOCOL_PATH" "$(TOP)/../../eurotherm2kApp/protocol"
-epicsEnvSet "TTY" "$(TTY=\\\\\\\\.\\\\COM19)"
+epicsEnvSet "TTY" "$(TTY=\\\\\\\\.\\\\COM18)"
 epicsEnvSet "SENS_DIR" "C:/InstrumentSettings/calib/sensors"
 
 cd ${TOP}
@@ -24,6 +24,11 @@ asynSetOption("L0", -1, "bits", "7")
 asynSetOption("L0", -1, "parity", "even")
 asynSetOption("L0", -1, "stop", "1")
 
+## Load ReadASCII
+## A seperate instance must be created for each eurotherm
+ReadASCIIConfigure("READASCII1")
+ReadASCIIConfigure("READASCII2")
+
 < $(IOCSTARTUP)/dbload.cmd
 
 ## Load the sim and disable records
@@ -35,8 +40,9 @@ dbLoadRecords("$(TOP)/db/devSimDis.db","Q=$(MYPVPREFIX)EUROTHERM:")
 ## LAD = Lesser Eurotherm address part
 ## For example: eurotherm address 1 => GAD = 0 and LAD = 1
 ## For example: eurotherm address 10 => GAD = 1 and LAD = 0
-dbLoadRecords("$(TOP)/db/devEurotherm.db","P=$(MYPVPREFIX)EUROTHERM1:, Q=$(MYPVPREFIX)EUROTHERM:, GAD=0, LAD=1, PORT=L0, SDIR=$(SENS_DIR)")
-dbLoadRecords("$(TOP)/db/devEurotherm.db","P=$(MYPVPREFIX)EUROTHERM2:, Q=$(MYPVPREFIX)EUROTHERM:, GAD=0, LAD=2, PORT=L0, SDIR=$(SENS_DIR)")
+dbLoadRecords("$(TOP)/db/devEurotherm.db","P=$(MYPVPREFIX)EUROTHERM1:, Q=$(MYPVPREFIX)EUROTHERM:, GAD=0, LAD=1, PORT=L0, SDIR=$(SENS_DIR), READ=READASCII1")
+dbLoadRecords("$(TOP)/db/devEurotherm.db","P=$(MYPVPREFIX)EUROTHERM2:, Q=$(MYPVPREFIX)EUROTHERM:, GAD=0, LAD=2, PORT=L0, SDIR=$(SENS_DIR), READ=READASCII2")
+
 
 < $(IOCSTARTUP)/preiocinit.cmd
 
