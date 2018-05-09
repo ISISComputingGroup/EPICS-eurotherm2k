95729ffad4ad052d2047dce0e4ec19e126a665a2
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-05-09 12:07:43 +0100

    Tidy

--- eurotherm2kApp/src/get_units_impl.cpp
+++ eurotherm2kApp/src/get_units_impl.cpp
@@ -6,7 +6,6 @@
 #include <errlog.h>
 #include <epicsString.h>
 #include <epicsExport.h>
-
 #include <string>
 #include <vector>
 #include <sstream>
@@ -24,17 +23,18 @@ std::string get_units_from_file(std::string filepath)
     std::string default_units = "K";
     std::string units = default_units;
     
-    std::cout << "Reading filepath: " << filepath << std::endl;
     std::ifstream infile(filepath);
     std::string line;
     while (std::getline(infile, line))
     {
-        if (line.substr(0, units_prefix.size()) == units_prefix) {
-            try {
+        try {
+            // Ideally would use line.starts_with but that's only in C++20
+            if (line.substr(0, units_prefix.size()) == units_prefix) {
                 units = line.substr(units_prefix.size(), line.size());
-            } catch (std::out_of_range) {
-                std::cerr << "Caught out of range error processing line: " << line << std::endl;
             }
+        } catch (std::out_of_range) {
+            // Out of range error thrown if line is shorter than len(units_prefix)
+            // Ignore this case
         }
     }
     return units;
@@ -56,11 +56,6 @@ int get_units_impl(aSubRecord *prec)
     std::string sensor_file = str_from_epics(prec->c);
     
     std::string units = get_units_from_file(base_dir + "/" + sensor_dir + "/" + sensor_file);
-    
-    std::cout << "Base dir is " << base_dir << std::endl;
-    std::cout << "Sensor dir is " << sensor_dir << std::endl;
-    std::cout << "Sensor file is " << sensor_file << std::endl;
-    std::cout << "Units are " << units << std::endl;
     
     strcpy(*(epicsOldString*) prec->vala, units.c_str());
     return 0;
